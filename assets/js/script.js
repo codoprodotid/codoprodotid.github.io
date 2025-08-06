dc7b228d60435dbf53a48f4e9617df02fdb4b3cd
O027-MIFTAHUL ULUM,S.Pd.+Gr <ulumiftahul06@gmail.com>
2025-08-06 11:21:06 +0700

    services

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,29 @@ const testimonials = [
     },
 ];
 
+const portfolios = [{ name: "Landing Page" }];
+
+const services = [
+    {
+        name: "Landing Page",
+        description: "Pembuatan landing page untuk produk atau jasa",
+        icon: "fa fa-solar-panel",
+        image: "./assets/img/logo.png",
+    },
+    {
+        name: "Website Profil Organisasi/Institusi",
+        description: "Pembuatan website profil organisas/institusi",
+        icon: "fa fa-solar-panel",
+        image: "./assets/img/logo.png",
+    },
+    {
+        name: "Konsultan IT",
+        description: "Konsultasi di Bidang IT",
+        icon: "fa fa-solar-panel",
+        image: "./assets/img/logo.png",
+    },
+];
+
 $(document).ready(function () {
     setTextElement(".app-name", config.app_name);
     setTextElement(".address", config.address);
@@ -34,12 +57,13 @@ $(document).ready(function () {
     setTextElement(".work-hour", config.work_hour);
     setTeams();
     setTestimonials();
+    setSevices();
 });
 
 function setTextElement(selector, text) {
     $(selector).text(text);
 }
-
+// teams
 function setTeams() {
     let result = "";
     teams.forEach((team, index) => {
@@ -68,7 +92,7 @@ function setTeamItem(team, index = 0) {
     </div>`;
     return html;
 }
-
+// testimonials
 function setTestimonials() {
     let html = "";
     testimonials.forEach((item, index) => {
@@ -89,6 +113,33 @@ function setTetimonyItem(item, index = 0) {
             <p>${item.testimony}</p>
             <h5 class="mb-1">${item.name}</h5>
             <span class="fst-italic">${item.profession}</span>
+        </div>
+    </div>`;
+    return html;
+}
+
+// services
+
+function setSevices() {
+    let result = "";
+    services.forEach((item, index) => {
+        result += setServiceItem(item, index);
+    });
+    $("#container-services").html(result);
+}
+
+function setServiceItem(item, index) {
+    let html = `<div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="service-item rounded overflow-hidden">
+            <img class="img-fluid" src="${item.image}" alt="">
+            <div class="position-relative p-4 pt-0">
+                <div class="service-icon">
+                    <i class="${item.icon} fa-3x"></i>
+                </div>
+                <h4 class="mb-3">${item.name}</h4>
+                <p>${item.description}</p>
+                <a class="small fw-medium d-none" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+            </div>
         </div>
     </div>`;
     return html;
